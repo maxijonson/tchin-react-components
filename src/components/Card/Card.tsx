a90e2a19e4d4afe152fb7a8229ad415da288c0c2
Tristan Chin <tristan.chin@chintristan.io>
2020-01-04 22:47:56 -0500

    Edit Card dark colors

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,10 @@
 import styled from "styled-components";
-import tinycolor from "tinycolor2";
 
 export default styled.div`
     display: inline-block;
     border-radius: 5px;
     padding: 1.6rem;
-    background: ${({ theme }) =>
-        tinycolor(theme.colors.pageBackground)
-            .clone()
-            .lighten(5)
-            .toHex8String()};
+    background: ${({ theme }) => theme.colors.card};
     margin: 2%;
     border: 1px solid ${({ theme }) => theme.colors.cardBorder};
     box-shadow: 0px 2px 2px -1px ${({ theme }) => theme.colors.cardShadow};
